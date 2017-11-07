761c8d3e879f09d8dd81d2575b90c750ceb7cfd6
Matthew Tremper <Matthew.Tremper@ipreo.com>
2017-11-06 19:14:43 -0500

    Add security questions controller

---
+++ src/Voidwell.Auth/Controllers/SecurityQuestionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Voidwell.Auth.Services;
+using System.Collections.Generic;
+
+namespace Voidwell.VoidwellAuth.Client.Controllers
+{
+    [Route("account/questions")]
+    public class SecurityQuestionsController : Controller
+    {
+        private readonly ISecurityQuestionService _securityQuestionService;
+
+        public SecurityQuestionsController(ISecurityQuestionService securityQuestionService)
+        {
+            _securityQuestionService = securityQuestionService;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetSecurityQuestions()
+        {
+            return _securityQuestionService.GetSecurityQuestionsList();
+        }
+    }
+}
